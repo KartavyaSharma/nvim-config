444a88fb8d4be2057ee14c76b1863a415804f0a0
Kartavya Sharma <kartavya@berkeley.edu>
2023-01-31 05:41:01 -0800

    plugins.lua format

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -40,84 +40,84 @@ packer.init({
 
 -- packer plugins
 local function plugins(use)
-	-- PACKER
-	use({ "wbthomason/packer.nvim" })
+    -- PACKER
+    use { 'wbthomason/packer.nvim' }
 
-	-- MISC
-	use({ "nvim-lua/plenary.nvim" }) -- useful lua functions
-	use({ "windwp/nvim-autopairs" }) -- auto closing
-	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- auto tag pairing
-	use({ "goolord/alpha-nvim" }) -- startup screen
-	use({ "echasnovski/mini.nvim", branch = "stable" }) -- library of mini plugins
+    -- MISC
+    use { 'nvim-lua/plenary.nvim' }                                 -- useful lua functions
+    use { 'windwp/nvim-autopairs' }                                 -- auto closing
+    use { 'windwp/nvim-ts-autotag', after='nvim-treesitter' }       -- auto tag pairing
+    use { 'goolord/alpha-nvim' }                                    -- startup screen
+    use { 'echasnovski/mini.nvim', branch = 'stable' }              -- library of mini plugins
 
-	-- COLORSCHEME
-	use({ "folke/tokyonight.nvim" })
-	use({ "lunarvim/darkplus.nvim" })
-	use({ "EdenEast/nightfox.nvim" })
+    -- COLORSCHEME
+    use { 'folke/tokyonight.nvim' }
+    use { 'lunarvim/darkplus.nvim' }
+    use { 'EdenEast/nightfox.nvim' }
 
-	-- EDITOR
-	use({ "numToStr/Comment.nvim" }) -- multiline comments
-	use({ "PHSix/faster.nvim" }) -- accelerated jk
-	use({ "nvim-tree/nvim-tree.lua", tag = "nightly" }) -- nvim-tree
-	use({ "nvim-tree/nvim-web-devicons" }) -- nvim-web-devicons
-	use({ "folke/todo-comments.nvim" }) -- TODO tree
-	use({ "nvim-lualine/lualine.nvim" }) -- status line
-	use({ "akinsho/bufferline.nvim", tag = "v3.*" }) -- bufferline when file edit
-	use({ "ahmedkhalf/project.nvim" }) -- superior project management
-	use({ "lukas-reineke/indent-blankline.nvim" }) -- vertical lines at each indentation level
-	use({ "famiu/bufdelete.nvim" }) -- easy buffer deletion
-	use({ "akinsho/toggleterm.nvim" }) -- integrated nvim terminal
-	use({ "folke/which-key.nvim" }) -- keymap integration
-	-- use { 'preservim/tagbar' }                                   -- tag bar with file structure
-	-- use { 'karb94/neoscroll.nvim' }                              -- smooth scrolling
-	-- use { 'feline-nvim/feline.nvim' }                            -- another status line
+    -- EDITOR
+    use { 'numToStr/Comment.nvim' }                                 -- multiline comments
+    use { 'PHSix/faster.nvim' }                                     -- accelerated jk
+    use { 'nvim-tree/nvim-tree.lua', tag='nightly' }                -- nvim-tree
+    use { 'nvim-tree/nvim-web-devicons' }                           -- nvim-web-devicons
+    use { 'folke/todo-comments.nvim' }                              -- TODO tree
+    use { 'nvim-lualine/lualine.nvim' }                             -- status line
+    use { 'akinsho/bufferline.nvim', tag='v3.*' }                   -- bufferline when file edit
+    use { 'ahmedkhalf/project.nvim' }                               -- superior project management
+    use { 'lukas-reineke/indent-blankline.nvim' }                   -- vertical lines at each indentation level      
+    use { 'famiu/bufdelete.nvim' }                                  -- easy buffer deletion
+    use { 'akinsho/toggleterm.nvim' }                               -- integrated nvim terminal
+    use { 'folke/which-key.nvim' }                                  -- keymap integration
+    -- use { 'preservim/tagbar' }                                   -- tag bar with file structure
+    -- use { 'karb94/neoscroll.nvim' }                              -- smooth scrolling
+    -- use { 'feline-nvim/feline.nvim' }                            -- another status line
 
-	-- GIT
-	use({ "lewis6991/gitsigns.nvim" }) -- git change signs on the left
+    -- GIT
+    use { 'lewis6991/gitsigns.nvim' }                               -- git change signs on the left
 
-	-- TELESCOPE
-	use({ "nvim-telescope/telescope.nvim" }) -- core
-	use({ "nvim-telescope/telescope-fzf-native.nvim", rum = "make" }) -- better fzf perf
-	use({ "junegunn/fzf" }) -- fuzzy finding
+    -- TELESCOPE
+    use { 'nvim-telescope/telescope.nvim' }                         -- core
+    use { 'nvim-telescope/telescope-fzf-native.nvim', rum='make' }  -- better fzf perf
+    use { 'junegunn/fzf' }                                          -- fuzzy finding
 
-	-- TREESITTER
-	use({ "nvim-treesitter/nvim-treesitter" }) -- better syntax highlighting
+    -- TREESITTER
+    use { 'nvim-treesitter/nvim-treesitter' }                       -- better syntax highlighting
 
-	-- CMP
-	use({ "hrsh7th/nvim-cmp" }) -- core completion plugin
-	use({ "hrsh7th/cmp-buffer" }) -- buffer completion support
-	use({ "hrsh7th/cmp-path" }) -- path completion support
-	use({ "hrsh7th/cmp-cmdline" }) -- vim cmd completion support
-	use({ "hrsh7th/cmp-nvim-lsp" }) -- lst completion integration with cmp
-	use({ "saadparwaiz1/cmp_luasnip" }) -- snippet completion support
+    -- CMP
+    use { "hrsh7th/nvim-cmp" }                                      -- core completion plugin
+    use { 'hrsh7th/cmp-buffer' }                                    -- buffer completion support
+    use { 'hrsh7th/cmp-path' }                                      -- path completion support
+    use { 'hrsh7th/cmp-cmdline' }                                   -- vim cmd completion support
+    use { 'hrsh7th/cmp-nvim-lsp' }                                  -- lst completion integration with cmp
+    use { 'saadparwaiz1/cmp_luasnip' }                              -- snippet completion support
 
-	-- SNIPPETS
-	use({ "L3MON4D3/LuaSnip", run = "make install_jsregexp" }) -- snippet engine with in-place transformations
-	use({ "KartavyaSharma/friendly-snippets" }) -- bunch of snippets
+    -- SNIPPETS
+    use { "L3MON4D3/LuaSnip", run="make install_jsregexp" }         -- snippet engine with in-place transformations
+    use { "KartavyaSharma/friendly-snippets" }                      -- bunch of snippets
 
-	-- LSP
-	use({ "williamboman/mason.nvim" }) -- plugin manager for lsp/daps
-	use({ "williamboman/mason-lspconfig.nvim" }) -- lsp config integration with mason
-	use({ "neovim/nvim-lspconfig" }) -- enables lsp
-	use({ "mfussenegger/nvim-jdtls" }) -- java language server
+    -- LSP
+    use { 'williamboman/mason.nvim' }                               -- plugin manager for lsp/daps
+    use { 'williamboman/mason-lspconfig.nvim' }                     -- lsp config integration with mason
+    use { 'neovim/nvim-lspconfig' }                                 -- enables lsp
+    use { 'mfussenegger/nvim-jdtls' }                               -- java language server
 
-	-- LINTING
-	use({ "jose-elias-alvarez/null-ls.nvim" }) -- lsp based code diagnostics and formatting
+    -- LINTING
+    use { 'jose-elias-alvarez/null-ls.nvim' }                       -- lsp based code diagnostics and formatting
 
-	-- MARKDOWN
-	use({ "iamcco/markdown-preview.nvim" }) -- browser based markdown preview for nvim
+    -- MARKDOWN
+    use { 'iamcco/markdown-preview.nvim' }                          -- browser based markdown preview for nvim
 
-	-- LATEX
-	use({ "lervag/vimtex" }) -- for working with latex in nvim
+    -- LATEX
+    use { 'lervag/vimtex' }                                         -- for working with latex in nvim
 
-	-- NAVIGATION
-	use({ "ggandor/leap.nvim" }) -- for moving anywhere
-	use({ "nanozuki/tabby.nvim" }) -- tabline plugin
+    -- NAVIGATION
+    use { 'ggandor/leap.nvim' }                                     -- for moving anywhere
+    use { 'nanozuki/tabby.nvim' }                                   -- tabline plugin
 
-	if packer_bootstrap then
-		print("Restart Neovim required after installation!")
-		require("packer").sync()
-	end
+    if packer_bootstrap then
+        print 'Restart Neovim required after installation!'
+        require('packer').sync()
+    end
 end
 
 -- call packer.startup with plugin function
